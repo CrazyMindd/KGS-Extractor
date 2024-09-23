7ec3938fa4fb03224344a029e7f58a8fcbcdc97d
CrazyMindd <vasnu333@gmail.com>
2024-09-23 12:06:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -88,3 +88,189 @@ async def restart_handler(_, m):
         )
     await m.reply_text("➭ 𝗕𝗼𝘁 𝗜𝘀 𝗕𝗲𝗶𝗻𝗴 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴. 𝗣𝗹𝗲𝗮𝘀𝗲 𝗞𝗲𝗲𝗽 𝗣𝗮𝘁𝗶𝗲𝗻𝗰𝗲", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+@bot.on_message(filters.command(["khan"]))
+async def khan_login(app, message):
+    input1 = await app.ask(message.chat.id, text="**Send ID & Password in this manner otherwise bot will not respond.\n\nSend like this:-  ID*Password**")
+
+    login_url = "https://khanglobalstudies.com/api/login-with-password"
+    raw_text = input1.text
+
+    headers = {
+        "Host": "khanglobalstudies.com",
+        "content-type": "application/x-www-form-urlencoded",
+        "content-length": "36",
+        "accept-encoding": "gzip",
+        "user-agent": "okhttp/3.9.1"
+    }
+
+    data = {
+        "password": "",
+        "phone": ""
+    }
+    data["phone"] = raw_text.split("*")[0]
+    data["password"] = raw_text.split("*")[1]
+    await input1.delete(True)
+
+    response = requests.post(login_url, headers=headers, data=data)
+    if response.status_code == 200:
+        data = response.json()
+        token = data["token"]
+        await message.reply_text("**Login Successful**")
+    else:
+        await message.reply_text("Go back to response")
+
+    headers = {
+        "Host": "khanglobalstudies.com",
+        "authorization": f"Bearer {token}",
+        "accept-encoding": "gzip",
+        "user-agent": "okhttp/3.9.1"
+    }
+
+    course_url = "https://khanglobalstudies.com/api/user/v2/courses"
+    response = requests.get(course_url, headers=headers)
+
+    data = response.json()
+    courses = [(course['id'], course['title']) for course in data]
+
+    FFF = "BATCH-ID  - BATCH-NAME\n\n"
+
+    for course_id, course_title in courses:
+        FFF += f"`{course_id}` - **{course_title}**\n\n"
+
+    await message.reply_text(FFF)
+
+    input3 = await app.ask(message.chat.id, text="**Now send the Batch ID to Download**")    
+    raw_text3 = input3.text
+    for course in data:
+        if course['id'] == raw_text3:
+            batch_name = course['title']
+        else:
+            batch_name = "KHAN-SIR"
+    url = "https://khanglobalstudies.com/api/user/courses/"+raw_text3+"/v2-lessons"
+    response2 = requests.get(url, headers=headers)
+    
+    msg = await message.reply_text("**Prepared your course id**")
+    bat_id = ""
+    for data in response2.json():
+        baid = f"{data['id']}&"
+        bat_id += baid
+          
+    await msg.edit_text("**Done your course id\n Now Extracting your course**")
+    full = ""
+    try:
+        xv = bat_id.split('&')
+        for y in range(0,len(xv)):
+            t =xv[y]
+            try:
+                url = "https://khanglobalstudies.com/api/lessons/"+t  
+                response = requests.get(url, headers=headers)
+                data = response.json()
+                
+        
+                videos = data.get('videos', [])
+                fuck = ""
+                for video in videos: 
+                    class_title = video.get('name')
+                    class_url = video.get('video_url')
+                    fuck += f"{class_title}: {class_url}\n"
+        
+                full += fuck
+            except Exception as e:
+                print(str(e))
+                pass
+                
+        with open(f"{batch_name}.txt", 'a') as f:
+            f.write(f"{full}")
+        
+        c_txt = f"**App Name: Khan-Sir\nBatch Name:** `{batch_name}`"
+        await message.reply_document(document=f"{batch_name}.txt", caption=c_txt)
+        os.remove(f"{batch_name}.txt")
+
+    except Exception as e:
+        await message.reply_text(str(e))
+
+@bot.on_message(filters.command(["adownload"]))
+async def account_ln(bot: Client, m: Message):
+    user = m.from_user.id if m.from_user is not None else None
+    if user is not None and user not in sudo_users:
+        await m.reply("**Buy it from @LegendBoy_OP**", quote=True)
+        return
+    else:
+        editable = await m.reply_text(
+            "Hello Bruh **I An Anurag Downloader Bot**. I can download videos from **text** file one by one.**\n\nLanguage** : Python**\nFramework** : Pyrogram\n\nSend **TXT** File {Name : Link}"
+        )
+    input: Message = await bot.listen(editable.chat.id)
+    x = await input.download()
+    await input.delete(True)
+
+    f"./downloads/{m.chat.id}"
+
+    try:
+        with open(x, "r") as f:
+            content = f.readlines()
+        os.remove(x)
+    except:
+        await m.reply_text("Invalid file input.")
+        os.remove(x)
+        return
+
+    editable = await m.reply_text(
+        f"Total Videos found in this Course are **{len(content)}**\n\nSend From where you want to download initial is **1**"
+    )
+    input1: Message = await bot.listen(editable.chat.id)
+    raw_text = input1.text
+
+    raw_text5 = input.document.file_name.replace(".txt", "")
+    await input.delete(True)
+    editable4 = await m.reply_text("**Send thumbnail url**\n\nor Send **no**")
+    input6 = message = await bot.listen(editable.chat.id)
+    input6.text
+
+    thumb = input6.text
+    if thumb.startswith("http://") or thumb.startswith("https://"):
+        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+        thumb = "thumb.jpg"
+    else:
+        thumb == "no"
+
+    try:
+        for count, i in enumerate(
+            range(int(raw_text) - 1, len(content)), start=int(raw_text)
+        ):
+            name1, link = content[i].split(":", 1)
+            cook, url = (
+                requests.get(f"https://api.telegramadmin.ga/gurukul/link={link}")
+                .json()
+                .values()
+            )
+
+            name = f"{str(count).zfill(3)}) {name1}"
+            Show = (
+                f"**Downloading:-**\n\n**Name :-** `{name}`\n\n**Url :-** `{url}`\n\n`"
+            )
+            prog = await m.reply_text(Show)
+            cc = f"**Name »** {name1}.mp4\n**Batch »** {raw_text5}\n**Index »** {str(count).zfill(3)}"
+            if "youtu" in url:
+                cmd = f'yt-dlp -f best "{url}" -o "{name}"'
+            elif "player.vimeo" in url:
+                cmd = f'yt-dlp -f "bestvideo+bestaudio" --no-keep-video "{url}" -o "{name}"'
+            else:
+                cmd = f'yt-dlp -o "{name}" --add-header "cookie: {cook}" "{url}"'
+            try:
+                res_file = await helper.download_video(url, cmd, name)
+                filename = res_file
+                await helper.send_vid(bot, m, cc, filename, thumb, name, prog)
+                count += 1
+
+                time.sleep(1)
+            except Exception as e:
+                await m.reply_text(
+                    f"**downloading failed ❌**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}`\n"
+                )
+                continue
+    except Exception as e:
+        await m.reply_text(str(e))
+    await m.reply_text("Done")
+
