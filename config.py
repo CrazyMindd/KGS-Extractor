efb5d9592850bc095c6a2d359c5345e1ef8f0d0d
CrazyMindd <vasnu333@gmail.com>
2024-09-27 21:31:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28528007
-api_hash = "38464da16c80310cabc8d13952419cf3"
-bot_token = "6946859945:AAEG0hlNK-ORA4VrwZ6GxwwN8ei4H80jDwI"
-auth_users = "6329158981"
+api_id = 28228907
+api_hash = "3848da16c807890cabc8d1395248c9cf3"
+bot_token = "6945259945:AAEG0hlngK-ORA4VrwZ6GxwybN8ei4H80jDwI"
+auth_users = "6127558921"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6329158981"
+osowner_users = "6127558921"
 owner_users = [int(num) for num in osowner_users.split(",")]
